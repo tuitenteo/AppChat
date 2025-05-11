19980bcc8016ecf9cdbf73550304297e0c21fb01
tien phan <ngocttt2020@gmail.com>
2025-05-11 23:43:48 +0700

    Thêm userId

--- Seminar/FE/context/AuthContext.js
+++ Seminar/FE/context/AuthContext.js
@@ -1,60 +1,69 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage'; // bộ nhớ vinhx viễn của thiết bị, nên có thể lưu token vào đây ngay cả khi user đóng ứng dụng
-const AuthContext = createContext(); // context api để quản lý trạng thái đăng nhập toàn ứng dụng
-export const AuthProvider = ({ children }) => { // AuthProvider là 1 component để bọc các component khác trong ứng dụng, cung cấp giá trị cho context ( đã bọc app.js ) để các component con khác biết đc là ai đang truy cập 
-  const [authState, setAuthState] = useState({
-    token: null, //user đã đăng nhập hay chưa
-    isLoading: true
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
+const AuthContext = createContext(); 
+
+export const AuthProvider = ({ children }) => { 
+  const [authState, setAuthState] = useState({  // theoi dõi trạng thái xác thực
+    token: null, 
+     userId: null,
+    isLoading: true 
   });
-  // Load token từ AsyncStorage khi mở app
   useEffect(() => {
-    const loadToken = async () => {
+    const loadToken = async () => { 
       try {
-        const storedToken = await AsyncStorage.getItem('token'); // khi mở app lên thì sẽ load token (đã đc lưu từ trc nếu có) từ AsyncStorage để app k bắt đăng nhập lại
-        setAuthState({
+        const storedToken = await AsyncStorage.getItem('token'); 
+        const storedNewUserId = await AsyncStorage.getItem('userId');
+      console.log("[AuthContext] Loaded userId:", storedNewUserId);
+        setAuthState({ 
           token: storedToken,
-          isLoading: false
+          userId: storedNewUserId,
+          isLoading: false 
         });
       } catch (err) {
         console.error("Lỗi khi load token:", err);
         setAuthState({
           token: null,
+          userId: null,
           isLoading: false
         });
       }
     };
-    loadToken();
+    loadToken(); 
   }, []);
 
-  const login = async (newToken) => {
-    await AsyncStorage.setItem('token', newToken); // khi đnăg nhập lưu token vào AsyncStorage để khi lần sau mở app lên không cần đăng nhập lại
-    setAuthState({
+  const login = async (newToken, newUserId) => {
+    await AsyncStorage.setItem('token', newToken); 
+    await AsyncStorage.setItem('userId', JSON.stringify(newUserId));
+    setAuthState({ 
       token: newToken,
+      userId: newUserId,
       isLoading: false
     });
   };
+  
   const logout = async () => {
-    await AsyncStorage.removeItem('token'); // và khi đăng xuất thì sẽ xóa token khỏi ÁsyncStorage\
-    setAuthState({
-      token: null,
+    await AsyncStorage.removeItem('token'); 
+    await AsyncStorage.removeItem('userId'); 
+    setAuthState({ 
+      token: null, 
+      userId: null,
       isLoading: false
     });
   };
   return (
-    //AuthProvider là phát dữ liệu đó cho các component con trong ứng dụng
-    //authcontext là context chính chứa dữ liệu muốn share
     <AuthContext.Provider value={{
       token: authState.token,
+       userId: authState.userId,
       isLoading: authState.isLoading,
       login,
       logout
     }}>
-      {/* component nào nằm trong children đều có thể truy cập dữ liệu mà value đang chia sẻ -- token, logout, login */}
-      {children}
+      {children} 
     </AuthContext.Provider>
   );
 };
 
 export const useAuth = () => useContext(AuthContext);
+//useContext là hook có sẵn của React để lấy dữ liệu từ một Context.
 
 
